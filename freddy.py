26b6b90296c85955c0de568c4c00fd6b4aac11a3
nmdeni <mekadeni@mail.ru>
2024-07-27 15:49:05 +0500

    переделал приветствие(сделал чтобы здоровался один раз при запуске) и и сказал об отключение программы

--- freddy.py
+++ freddy.py
@@ -3,30 +3,34 @@ class Freddy:
     """Основной класс помощника Freddy"""
 
     def __init__(self,status):
+        self.status = status
         self.name = 'Freddy'
         self.age = f"{int(datetime.date.today().year) - 2024} лет"
-        self.status = status
+        self.name_dev = "Мой создатель NMD"
+        self.friends_list =
         self.menu = [
             'q - выключить Freddy',
             'name - узнать имя',
             'age - узнать возраст'
         ]
-
-        self.startFreddy()
+        if self.status:
+            self.startFreddy()
     def startFreddy(self):
+        print(f"Привет, я {self.name}! Вот что я умею\n")
+
         while(self.status):
-            print(f"Привет, я {self.name}! Вот что я умею\n")
 
             for i in self.menu:
                 print(i)
             user_com = input("\nВведите команду > ")
 
             if user_com == 'q':
+                print('Отключение...')
                 self.status = False
             elif user_com == 'name':
-                print(self.name)
+                print(self.name + '\n')
             elif user_com == 'age':
-                print(self.age)
+                print(self.age + '\n')
             else:
                 print("Я еще этого не умею")
 
