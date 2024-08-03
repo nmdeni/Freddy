c1c698290c5314cfeee533c94ea4c7df2a781695
nmdeni <mekadeni@mail.ru>
2024-08-03 19:32:28 +0500

    подправил задачи, запуск, и начал работу с подклчением к бд

--- freddy.py
+++ freddy.py
@@ -8,13 +8,31 @@ class Freddy:
         self.name = 'Freddy'
         self.age = f"{int(datetime.date.today().year) - 2024} лет"
         self.name_dev = "Мой создатель NMD"
-        self.friends_list =
+        self.conn_db = ''
+        self.erro_conn_db = 'Не удалось подключиться с базе данных!!!'
+        # self.friends_list =
         self.menu = [
             'q - выключить Freddy',
             'name - узнать имя',
             'age - узнать возраст'
         ]
         if self.status:
+            print('Подключение к базе данных')
+            user_info_db = {
+                'dbname':'',
+                'user':'',
+                'password':'',
+                'host':''
+            }
+
+            for k in user_info_db.keys():
+                print(k)
+                user_info_db[k] = input('> ')
+            try:
+                # self.conn_db = psycopg2.connect(dbname='freddy', user='postgres', )
+                print(user_info_db)
+            except:
+                print(self.erro_conn_db)
             self.startFreddy()
     def startFreddy(self):
         print(f"Привет, я {self.name}! Вот что я умею\n")
@@ -36,5 +54,5 @@ class Freddy:
                 print("Я еще этого не умею")
 
 if __name__ == '__main__':
-    user_com = input("включить Freddy (y/n)? > ")
+    user_com = input("включить Freddy (y = Да)? > ")
     freddy = Freddy(True if user_com == "y" else False)
