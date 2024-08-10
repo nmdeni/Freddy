61dba1585259242e9050387b6264b908ac021e64
nmdeni <mekadeni@mail.ru>
2024-08-10 16:53:16 +0500

    настроил подключение к базе данных

--- freddy.py
+++ freddy.py
@@ -8,33 +8,16 @@ class Freddy:
         self.name = 'Freddy'
         self.age = f"{int(datetime.date.today().year) - 2024} лет"
         self.name_dev = "Мой создатель NMD"
-        self.conn_db = ''
-        self.erro_conn_db = 'Не удалось подключиться с базе данных!!!'
         # self.friends_list =
         self.menu = [
             'q - выключить Freddy',
             'name - узнать имя',
             'age - узнать возраст'
         ]
-        if self.status:
-            print('Подключение к базе данных')
-            user_info_db = {
-                'dbname':'',
-                'user':'',
-                'password':'',
-                'host':''
-            }
 
-            for k in user_info_db.keys():
-                print(k)
-                user_info_db[k] = input('> ')
-            try:
-                # self.conn_db = psycopg2.connect(dbname='freddy', user='postgres', )
-                print(user_info_db)
-            except:
-                print(self.erro_conn_db)
-            self.startFreddy()
+        self.startFreddy()
     def startFreddy(self):
+        self.connect_db()
         print(f"Привет, я {self.name}! Вот что я умею\n")
 
         while(self.status):
@@ -52,6 +35,26 @@ class Freddy:
                 print(self.age + '\n')
             else:
                 print("Я еще этого не умею")
+
+    def connect_db(self):
+        print('Подключение к базе данных')
+        user_info_db = {
+            'user': '',
+            'dbname': '',
+            'password': ''
+        }
+
+        for k in user_info_db.keys():
+            print(k)
+            user_info_db[k] = input('> ')
+        try:
+            self.conn_db = psycopg2.connect(
+                dbname=user_info_db['dbname'],
+                user=user_info_db['user'],
+                password=user_info_db['password'])
+            print('Подключение прошло успешно!!!')
+        except:
+            print('Не удалось подключиться к базе данных!!!')
 
 if __name__ == '__main__':
     user_com = input("включить Freddy (y = Да)? > ")
